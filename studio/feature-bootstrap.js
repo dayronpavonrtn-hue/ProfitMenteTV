1c964b0818499a28330001511e41a02b18d27dbd
dayronpavonrtn-hue <dayronpavonrtn@gmail.com>
2026-09-09 03:36:55 -0400

    Wire render snapshot protection into Studio bootstrap

--- studio/feature-bootstrap.js
+++ studio/feature-bootstrap.js
@@ -91,6 +91,7 @@
     ['render-job-client.js','ProfitMenteRenderJobClient'],
     ['render-job-integration.js','ProfitMenteRenderJobs'],
     ['render-media-pruner.js','ProfitMenteRenderMediaPruner'],
+    ['render-snapshot-engine.js','ProfitMenteRenderSnapshotEngine'],
     ['render-recovery-integration.js','ProfitMenteRenderRecovery'],
     ['render-progress-engine.js','ProfitMenteRenderProgressEngine'],
     ['render-progress-integration.js','ProfitMenteRenderProgress'],
