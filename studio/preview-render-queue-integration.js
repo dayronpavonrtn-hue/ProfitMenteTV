3c7a539d49551dbfe4c989b66aac4e1bd93220b4
dayronpavonrtn-hue <dayronpavonrtn@gmail.com>
2026-09-12 07:38:22 -0400

    chore(studio): remove redundant preview render integration

--- studio/preview-render-queue-integration.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-  if(!globalThis.ProfitMentePreviewRenderQueue||typeof renderAt!=='function')return;
-  const baseRenderAt=renderAt;
-  const queue=new globalThis.ProfitMentePreviewRenderQueue(baseRenderAt);
-  renderAt=time=>queue.request(time);
-  globalThis.ProfitMentePreviewRenderQueueRuntime=queue;
-})();
