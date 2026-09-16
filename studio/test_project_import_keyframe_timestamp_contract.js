a49f633d609618fcd06f6177296d79795a0a6bbb
dayronpavonrtn-hue <dayronpavonrtn@gmail.com>
2026-09-16 00:34:23 -0400

    test(studio): cover ambiguous imported keyframe timestamps

---
+++ studio/test_project_import_keyframe_timestamp_contract.js
@@ -0,0 +1,12 @@
+const assert=require('assert');
+const {ProfitMenteProjectImportEngine}=require('./project-import-engine.js');
+const engine=new ProfitMenteProjectImportEngine();
+const base={version:'1.3',name:'Keyframe timestamps',mode:'Manual',duration:10,format:'9:16',fps:30,renderQuality:'high',clips:[{id:'clip-1',track:0,start:0,duration:5,asset:'media-1'}]};
+const withFrames=visualKeyframes=>({...base,clips:[{...base.clips[0],visualKeyframes}]});
+const sorted=engine.normalize(withFrames([{time:4,x:20},{time:1,x:-20}])).clips[0].visualKeyframes;
+assert.deepStrictEqual(sorted.map(frame=>frame.time),[1,4]);
+assert.throws(()=>engine.normalize(withFrames([{time:1,x:0},{time:1,x:10}])),/keyframe duplicados o ambiguos/i);
+assert.throws(()=>engine.normalize(withFrames([{time:1,x:0},{time:1.0009,x:10}])),/keyframe duplicados o ambiguos/i);
+const distinct=engine.normalize(withFrames([{time:1,x:0},{time:1.0011,x:10}])).clips[0].visualKeyframes;
+assert.equal(distinct.length,2);
+console.log('project import keyframe timestamp contract: ok');
